9ae4e2af01c6f27cc6bffa48c0074862c8ca31c2
Mykhailo Sytnyk <mykhailo.sytnyk.2@student.tuke.sk>
2025-11-29 14:51:46 +0100

    2

--- app/main.py
+++ app/main.py
@@ -1,10 +1,28 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
 
 app = FastAPI()
 
 
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
 
-@app.get("/pong")
-async def test():
-    return {"data": "Hello world"}
+client = OpenAI(api_key=api_key)
+@app.get("/ask")
+def ask_gpt(prompt: str = Query(...)):
+    print(">>> PROMPT FROM CLIENT:", prompt)
+
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    answer = completion.choices[0].message.content
+    print(">>> GPT ANSWER:", answer)
+
+    return {"answer": answer}
